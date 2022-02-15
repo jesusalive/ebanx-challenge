f97ae2146c48ad92b7dab7e498f0d8b6a3eaf8ce
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2022-02-15 16:01:17 -0300

    feat: add LoadAccountByIdRepository memory-mock implementation

--- src/Infrastructure/db/account/MemoryAccountRepository.ts
+++ src/Infrastructure/db/account/MemoryAccountRepository.ts
@@ -2,10 +2,18 @@ import { Account } from '@/Domain/account/Account'
 import { databaseAccounts } from '@/Infrastructure/db/db-memory-mock'
 import { DropAllDataRepository } from '@/Domain/shared/repositories/DropAllDataRepository'
 import { StoreAccountRepository } from '@/Domain/account/repositories/StoreAccountRepository'
+import { LoadAccountByIdRepository } from '@/Domain/account/repositories/LoadAccountByIdRepository'
 
-export class MemoryAccountRepository implements StoreAccountRepository, DropAllDataRepository {
+export class MemoryAccountRepository implements StoreAccountRepository, DropAllDataRepository, LoadAccountByIdRepository {
   async store (account: Account): Promise<void> {
     databaseAccounts.push(account)
+  }
+
+  async loadById (id: string): Promise<Account> {
+    const account = databaseAccounts.find(dbAccount => dbAccount.getId() === id)
+    if (account === undefined) return null
+
+    return account
   }
 
   async dropAllData (): Promise<void> {
