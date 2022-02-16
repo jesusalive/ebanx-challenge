abed3ca070acddd08bdf3c7336ada300c74433f8
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2022-02-16 10:13:56 -0300

    feat: add update balance step

--- src/Application/account/deposit/DbDeposit.ts
+++ src/Application/account/deposit/DbDeposit.ts
@@ -2,10 +2,12 @@ import { Account } from '@/Domain/account/Account'
 import { NotFoundError } from '@/Domain/shared/errors'
 import { Deposit } from '@/Domain/account/usecases/Deposit'
 import { LoadAccountByIdRepository } from '@/Domain/account/repositories/LoadAccountByIdRepository'
+import { UpdateAccountByIdRepository } from '@/Domain/account/repositories/UpdateAccountByIdRepository'
 
 export class DbDeposit implements Deposit {
   constructor (
-    private readonly loadAccountByIdRepository: LoadAccountByIdRepository
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+    private readonly updateAccountByIdRepository: UpdateAccountByIdRepository
   ) {}
 
   async do (amount: number, accountId: string): Promise<Account> {
@@ -13,6 +15,10 @@ export class DbDeposit implements Deposit {
     if (account === null) throw new NotFoundError('Account not found')
 
     account.makeDeposit(amount)
+    await this.updateAccountByIdRepository.updateById(accountId, {
+      balance: account.getBalance()
+    })
+
     return account
   }
 }
