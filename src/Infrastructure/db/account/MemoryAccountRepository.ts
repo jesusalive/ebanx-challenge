fecf7f2ee5bcdcddda31fe20fb6efdb0e84e6597
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2022-02-16 19:23:53 -0300

    feat: add UpdateAccountByIdRepository memory-mock implementation

--- src/Infrastructure/db/account/MemoryAccountRepository.ts
+++ src/Infrastructure/db/account/MemoryAccountRepository.ts
@@ -3,10 +3,20 @@ import { databaseAccounts } from '@/Infrastructure/db/db-memory-mock'
 import { DropAllDataRepository } from '@/Domain/shared/repositories/DropAllDataRepository'
 import { StoreAccountRepository } from '@/Domain/account/repositories/StoreAccountRepository'
 import { LoadAccountByIdRepository } from '@/Domain/account/repositories/LoadAccountByIdRepository'
+import { UpdateAccountByIdRepository, UpdateAccountData } from '@/Domain/account/repositories/UpdateAccountByIdRepository'
 
-export class MemoryAccountRepository implements StoreAccountRepository, DropAllDataRepository, LoadAccountByIdRepository {
+export class MemoryAccountRepository implements
+  StoreAccountRepository,
+  DropAllDataRepository,
+  LoadAccountByIdRepository,
+  UpdateAccountByIdRepository {
   async store (account: Account): Promise<void> {
     databaseAccounts.push(account)
+  }
+
+  async updateById (id: string, data: UpdateAccountData): Promise<void> {
+    const account = databaseAccounts.find(dbAccount => dbAccount.getId() === id)
+    account.setBalance(data.balance)
   }
 
   async loadById (id: string): Promise<Account> {
